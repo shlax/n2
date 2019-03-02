27025df2ed5cc3b01d00f87e70020d4a572cb7fd
slax <slax.10101@gmail.com>
2019-03-03 00:19:01 +0100

    .

--- src/main/java/org/n2/N2Store.java
+++ src/main/java/org/n2/N2Store.java
@@ -3,8 +3,6 @@ package org.n2;
 import org.h2.mvstore.MVMap;
 import org.h2.value.VersionedValue;
 
-import java.util.Objects;
-
 public final class N2Store<K> {
 
     final String name;
@@ -28,16 +26,4 @@ public final class N2Store<K> {
         return generator.generate();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        N2Store n2Store = (N2Store) o;
-        return name.equals(n2Store.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
